15042f55e6a3e8be853d39c79e36b9fd34138515
Nehil Danis <nehild@outlook.com>
2024-10-29 07:28:10 +0100

    ADDED: function definitions along with documentation

--- include/blur_detection/blur_detection.h
+++ include/blur_detection/blur_detection.h
@@ -1,5 +1,5 @@
-#ifndef BLUR_DETECTION_H    // Check if MY_HEADER_H is not defined
-#define BLUR_DETECTION_H    // Define MY_HEADER_H to prevent future inclusions
+#ifndef BLUR_DETECTION_H  // Check if MY_HEADER_H is not defined
+#define BLUR_DETECTION_H  // Define MY_HEADER_H to prevent future inclusions
 
 // STL
 #include <optional>
@@ -7,10 +7,93 @@
 // OpenCV
 #include <opencv2/core.hpp>
 
-namespace blur_detection {
-    inline auto is_blur() -> std::optional<float> {
-        return std::nullopt;
-    }
-} // namespace blur_detection
+namespace blur_detection
+{
+namespace detail
+{
 
-#endif // BLUR_DETECTION_H
+/**
+ * @brief This function takes a source image and applies one level of the haar
+ * filter and outputs low and high spatial resolution outputs.
+ *
+ * @param LL
+ * @param LH
+ * @param HL
+ * @param HH
+ */
+inline auto haar_transform(cv::InputArray src, cv::InputOutputArray LL,
+                           cv::InputOutputArray LH, cv::InputOutputArray HL,
+                           cv::InputOutputArray HH) -> void
+{
+}
+
+/**
+ * @brief This functions takes the high vertical, horizontal and diagonal
+ * information images, and constructs an edge map. All three matrices, LH, HL,
+ * and HH, should have the same shape. The formula used to consturct edge map is
+ * as follows sqrt(LH^2 + HL^2 + HH^2)
+ *
+ * @param LH
+ * @param HL
+ * @param HH
+ * @param edge_map
+ */
+inline auto calculate_edge_map(cv::InputArray LH, cv::InputArray HL,
+                               cv::InputArray HH,
+                               cv::OutputArray edge_map) -> void
+{
+}
+
+/**
+ * @brief This function calculates the maximum values in the edge map, using a
+ * filter_size x filter_size filter. The step size is equal to the filter_size.
+ * So we get a coaser map than the input edge_map. The maximum edge_map will be
+ * the size of floor(edge_map/filter_size)
+ *
+ * @param edge_map
+ * @param filter_size
+ * @param max_edge_map
+ */
+inline auto calculate_max_edge_map(cv::InputArray edge_map, size_t filter_size,
+                                   cv::OutputArray max_edge_map) -> void
+{
+}
+
+}  // namespace detail
+
+/**
+ * @brief This function checks if the image is blurry or not. If the image is
+ * blurry the blur extent will be returned.
+ *
+ * @param img Image to check the blurriness
+ * @param threshold This value can be in [0, 255]. But since the human eye
+ * cannot distinguish gray scale value below 30, providing a value above that is
+ * reasonable.
+ * @param min_zero This value cannot be lower than 0. This value is used to
+ * compare against the ratio of number of dirac and astep stuctures in all the
+ * edges. If the ratio is larger than the min_zero value than the image is
+ * considered un-blurred. In the blurred version there should not be any dirac
+ * or astep structures.
+ *
+ * @returns The blur extent in case of a blurry image, otherwise nullopt.
+ */
+inline auto is_blur(cv::InputArray img, float threshold,
+                    float min_zero) -> std::optional<float>
+{
+    // conver the image to gray scale, and the type to float
+
+    // 3-Level Haar Transform
+
+    // find edge_maps on these 3 levels
+
+    // find maximum edge_map for all levels using 8x8, 4x4, and 2x2 filters,
+    // from largest image to smallest
+
+    // Apply algorithm 2 to see if the image is blurry or not
+    // if yes to what extent
+
+    return std::nullopt;
+}
+}  // namespace blur_detection
+
+#endif  // BLUR_DETECTION_H
--- test/test_blur_detection.cpp
+++ test/test_blur_detection.cpp
@@ -1,9 +1,22 @@
 
-// example_test.cpp
+// GTest
 #include <gtest/gtest.h>
+
+// OpenCV
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+
 #include <blur_detection/blur_detection.h>
 
+
 TEST(BlurDetectionUsingHaarWaveletTransforms, testBlurriness) {
-    auto output = blur_detection::is_blur();
+
+    const auto original_image_path = "test/data/lena.png";
+    cv::Mat img = cv::imread(original_image_path, cv::IMREAD_COLOR);
+    static constexpr float threshold = 35;
+    static constexpr float min_zero = 0.05;
+
+
+    auto output = blur_detection::is_blur(img, threshold, min_zero);
     ASSERT_FALSE(output.has_value());
 }
